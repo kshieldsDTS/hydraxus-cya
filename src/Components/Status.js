7e8a4df8a23c427a83486cdd5b04a1965e37ad22
kshieldsDTS <escanor05@gmail.com>
2022-03-08 12:52:58 -0500

    initial commit

---
+++ src/Components/Status.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Status(props) {
+    return (
+        <div>
+            <h1>Status.js</h1>
+        </div>
+    );
+}
+
+export default Status;
---
+++ src/Components/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Welcome(props) {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <p>So begins your journey in Hydraxus...</p>
+        </div>
+    );
+}
+
+export default Welcome;
