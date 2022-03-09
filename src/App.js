646bf9537be33d26bd80490bf97b7ab00dd055b4
kshieldsDTS <escanor05@gmail.com>
2022-03-09 14:07:29 -0500

    write initial narrative, add navigation to next page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Status from './Components/Status'
 import Welcome from './Components/Welcome'
+import C1P2 from './Components/c1p2'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Status/>
       <Routes>
         <Route path='/' element={<Welcome/>}/>
+        <Route path='/c1p2' element={<C1P2/>}/>
       </Routes>
     </div>
   );
---
+++ src/Components/c1p2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function c1p2(props) {
+    return (
+        <div>
+            <h1>Chapter 1 Page 2</h1>
+            <p>The star grows larger in your view, slowly at first, but quickly appears to engulf all those around it. Within moments it dwarfs the moon, and it gains a fiery tail of red and white streaks. You watch as it draws closer, and the clouds part as it crashes through them an into the ocean. The shockwave knocks you from your feet, and all you can hear now is a ringing in your ears. You get to your feet and return to the cliff's edge, where you see a wave, at least one-hundred feet tall, rise from the ocean and slam into the coast below you. </p>
+            <p>Though you survived the cataclysm, you can't help but feel this isn't over, as you look toward the impact point, and see a frothing whirlpool in its wake. After a few moments, the water calms, and aside from the damage to the coast, nothing seems out of place.</p>
+        </div>
+    );
+}
+
+export default c1p2;
