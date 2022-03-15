012b985c19c29b727104634e046df7616dc82b76
kshieldsDTS <escanor05@gmail.com>
2022-03-15 18:47:09 -0400

    add new component

---
+++ src/Components/Chapter 1/C1P3A.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function C1P3A(props) {
+    return (
+        <div>
+            Looking out the window, you see an ocean, similar to the one in your dream. Like at the end of the dream, there appeared to be no sign of any celestial impact.
+        </div>
+    );
+}
+
+export default C1P3A;
