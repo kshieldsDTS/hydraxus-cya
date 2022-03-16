9865f1e6ae3963a47c2fd8d447a98b25e6a9c9e4
kshieldsDTS <escanor05@gmail.com>
2022-03-16 13:36:10 -0400

    add new page

--- src/Components/Chapter 1/C1P3A.js
+++ src/Components/Chapter 1/C1P3A.js
@@ -3,7 +3,7 @@ import React from 'react';
 function C1P3A(props) {
     return (
         <div>
-            Looking out the window, you see an ocean, similar to the one in your dream. Like at the end of the dream, there appeared to be no sign of any celestial impact.
+            Looking out the window, you see an ocean, similar to the one in your dream. Like at the end of the dream, there appeared to be no sign of any celestial impact. 
         </div>
     );
 }
---
+++ src/Components/Chapter 1/C1P3B.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function C1P3B(props) {
+    return (
+        <div>
+            The room measures six feet by six feet. The walls are made of large solid stone bricks. A single metal door is built into the wall, opposite the window.
+        </div>
+    );
+}
+
+export default C1P3B;
