c5d89e35044a2e43d6c5b8532cccd3a03ae7586a
kshieldsDTS <escanor05@gmail.com>
2022-03-18 14:14:32 -0400

    add narrative page

---
+++ src/Components/Chapter 1/C1P3C.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function C1P3C(props) {
+    return (
+        <div>
+            The door, built out of solid metal, stands unmoving and uncaring. You fiddle with the handle, but it does not move. You are locked in to the room.
+        </div>
+    );
+}
+
+export default C1P3C;
