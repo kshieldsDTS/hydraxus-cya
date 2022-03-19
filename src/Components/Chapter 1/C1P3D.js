d2d989e57b9c33a2547f1318fd520f95848ba6eb
kshieldsDTS <escanor05@gmail.com>
2022-03-19 12:55:09 -0400

    add narrative page

---
+++ src/Components/Chapter 1/C1P3D.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function C1P3D(props) {
+    return (
+        <div>
+            You scour through the bedding to see if there is anything you can use to escape this room. You find a single metal pick with a piece of paper rolled around it.
+
+            You unfurl the paper, and it reads, in your handwriting: "Figured you'd need this. You probably don't really know what's happening, but keep going. It'll all make sense soon."
+        </div>
+    );
+}
+
+export default C1P3D;
