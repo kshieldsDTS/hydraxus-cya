f93a45505ae6ad34d6642e7d2fd8488fd38789e8
kshieldsDTS <escanor05@gmail.com>
2022-03-20 12:30:12 -0400

    add narrative page

---
+++ src/Components/Chapter 1/C1P4.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function C1P4(props) {
+    return (
+        <div>
+            You manage to dig the pick into the lock. After fumbling about for a moment, you finally catch the tumbler. As you hear a satisfying click, the door swings freely.
+        </div>
+    );
+}
+
+export default C1P4;
