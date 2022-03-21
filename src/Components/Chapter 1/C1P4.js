117ae941d123d41b090ec9b4aaf2deb91a653bc2
kshieldsDTS <escanor05@gmail.com>
2022-03-21 10:41:38 -0400

    add to narrative

--- src/Components/Chapter 1/C1P4.js
+++ src/Components/Chapter 1/C1P4.js
@@ -3,7 +3,9 @@ import React from 'react';
 function C1P4(props) {
     return (
         <div>
-            You manage to dig the pick into the lock. After fumbling about for a moment, you finally catch the tumbler. As you hear a satisfying click, the door swings freely.
+            You manage to dig the pick into the lock. After fumbling about for a moment, you finally catch something. As you hear a satisfying click, the door swings open freely.
+
+            You cautiously open the door and peer out into the area outside your room. You see a long hallway with several doors.
         </div>
     );
 }
