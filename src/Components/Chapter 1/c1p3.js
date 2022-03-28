d64a0a942218606cfaa68c953f3f01b7cd3c16f1
kshieldsDTS <escanor05@gmail.com>
2022-03-28 12:37:17 -0400

    initial prototype of code. need to find way to get state to update after a single submit

--- src/Components/Chapter 1/c1p3.js
+++ src/Components/Chapter 1/c1p3.js
@@ -1,30 +1,78 @@
 import React from 'react';
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import C1P3A from './C1P3A';
+import C1P3B from './C1P3B';
+import C1P3C from './C1P3C';
 
 function C1P3(props) {
     const [userInput, setUserInput] = useState('')
     function handleChange(ev){
         setUserInput(ev.target.value)
     }
+    const [window, setWindow] = useState(false)
+    const [check, setCheck] =
+    useState(false)
+    const [door, setDoor] = useState(false)
+    const [room, setRoom] = useState(false)
+    const [page, setPage] = useState()
     function handleSubmit(ev){
         if (userInput)
         ev.preventDefault();
-        console.log('submitted!');
-        console.log(userInput);
+        const array = userInput.split(" ")
+        setCheck(false)
+        setWindow(false)
+        setRoom(false)
+        setDoor(false)
+        setPage('')
+        for (let i = 0; i<array.length; i++) {
+            if (array[i] === 'check') {
+                setCheck(true)
+            }
+            if (array[i] === 'window') {
+                setWindow(true)
+            }
+            if (array[i] === 'room') {
+                setRoom(true)
+            }
+            if (array[i] === 'door') {
+                setDoor(true)
+            }
+        }
+        checkInput()
+    }
+    function checkInput() {
+        if (window === true && check === true) {
+            setPage('a')
+        }
+        if (room === true && check === true) {
+			setPage('b');
+		}
+        if (door === true && check === true) {
+			setPage('c');
+		}
     }
     // add logic to find keywords in input and make move based on that.
     // Not sure how to implement. Convert string to array, then parse the array?
     return (
-        <div>
-            <p>Your eyes open, and you find yourself in a cold, dimly lit room. You sit up from the simple mat of straw and tattered cloth. A single window with a pair of iron bars allows sunlight to filter in to the room.</p>
-            <form onSubmit={handleSubmit}>
-                <legend>What do you do?</legend>
-                <input type='text' onChange={handleChange}></input>
-                <input type='submit' ></input>
-            </form>
-        </div>
-    );
+			<div>
+				<p>
+					Your eyes open, and you find yourself in a cold, dimly lit room. You
+					sit up from the simple mat of straw and tattered cloth. A single
+					window with a pair of iron bars allows sunlight to filter in to the
+					room.
+				</p>
+				<form onSubmit={handleSubmit}>
+					<legend>What do you do?</legend>
+					<input type='text' onChange={handleChange}></input>
+					<input type='submit'></input>
+				</form>
+				<br />
+				{page === 'a' ? <C1P3A /> : null}
+				{page === 'b' ? <C1P3B /> : null}
+				{page === 'c' ? <C1P3C /> : null}
+			</div>
+		);
 }
 
 export default C1P3;
